100a4d15feda29f0a253ce944b6bc8bab480e92c
Ritik Mishra <ritik.mishra314@gmail.com>
2020-11-12 23:23:57 -0600

    reformat code

--- anacreonlib/exceptions.py
+++ anacreonlib/exceptions.py
@@ -3,16 +3,17 @@ import requests
 
 class AuthenticationException(Exception):
     """Thrown when we have issues authenticating"""
+
     def __init__(self, *args, **kwargs):
         Exception.__init__(self, *args, **kwargs)
 
 
 class HexArcException(Exception):
     """Thrown when the server gets mad at us"""
+
     def __init__(self, *args, **kwargs):
         Exception.__init__(self, *args, **kwargs)
 
 
 if __name__ == "__main__":
-    requests.post("http://anacreon.kronosaur.com/api/getObjects",
-                  data={}).json()
+    requests.post("http://anacreon.kronosaur.com/api/getObjects", data={}).json()
